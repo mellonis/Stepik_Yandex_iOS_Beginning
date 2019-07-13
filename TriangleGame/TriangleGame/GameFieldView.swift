ce6512bd226a2be555f498b6f96d8665fc325279
mellonis <mellonis@yandex.ru>
2019-07-14 02:05:37 +0500

    Third block

--- TriangleGame/TriangleGame/GameFieldView.swift
+++ TriangleGame/TriangleGame/GameFieldView.swift
@@ -87,9 +87,9 @@ import UIKit
     }
     
     func setupViews() {
-        self.layer.borderWidth = 1
-        self.layer.cornerRadius = 8
-        self.layer.borderColor = UIColor.gray.cgColor
+        layer.borderWidth = 1
+        layer.cornerRadius = 8
+        layer.borderColor = UIColor.gray.cgColor
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
